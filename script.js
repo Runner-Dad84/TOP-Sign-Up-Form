d0886f82bd3110fc3162187cbbc96cdb8c60b423
Runner-Dad84 <vanhoogie@gmail.com>
2023-06-22 15:58:06 -0400

    Fix logo sizing.

--- script.js
+++ script.js
@@ -1,16 +1,42 @@
 let password = document.getElementById("password");
 let confirm = document.getElementById("confirm");
+let notifyPassword = document.getElementById("notifyPassword");
+let notifyConfirm = document.getElementById("notifyConfirm");
 
 password.addEventListener('change', (e) => {
     if (password.value === confirm.value){
-        console.log("pass match")
-        }else{
-            console.log("pass does not match")
+        notifyPassword.textContent = "Passwords match!"
+        notifyConfirm.textContent = "Passwords match!"
+        } else {
+            notifyPassword.textContent = "Passwords do not match!"
+            notifyConfirm.textContent = "Passwords do not match!"
 };})
 
 confirm.addEventListener('change', (e) => {
         if (confirm.value === password.value){
-            console.log("match")
+            notifyPassword.textContent = "Passwords match!"
+            notifyConfirm.textContent = "Passwords match!"
             }else{
-                console.log("Does not match")
+                notifyPassword.textContent = "Passwords do not match!"
+                notifyConfirm.textContent = "Passwords do not match!"
 };})
+
+
+
+/*
+let passwords = document.querySelectorAll("passwords");
+
+passwords.forEach(password => {
+    password.addEventListener('change', (e) => {
+    if (password.value === confirm.value){
+        notifyPassword.textContent = "Passwords match!"
+        notifyConfirm.textContent = "Passwords match!"
+        console.log("password matches")
+        } else {
+            notifyPassword.textContent = "Passwords do not match!"
+            notifyConfirm.textContent = "Passwords do not match!"
+            console.log("password do not matches")
+};})
+})
+
+*/
